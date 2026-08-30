a4ff2ac96eca73b0b46e2203ad52dcc8ffd8a034
gcvbbhhhxff-sys <gcvbbhhhxff@gmail.com>
2026-08-30 20:16:47 -0300

    feat: connect admin branding to public storefront

--- src/app/(cliente)/page.tsx
+++ src/app/(cliente)/page.tsx
@@ -1,4 +1,14 @@
 import { getPublicMenu } from "@/server/menu";
 import { MenuBrowser } from "@/components/cliente/menu-browser";
+import { RestaurantBrandProvider } from "@/components/cliente/restaurant-brand-provider";
+
 export const dynamic = "force-dynamic";
-export default async function ClientHomePage(){const data=await getPublicMenu();return <div className="min-h-screen"><MenuBrowser {...data}/></div>}
+
+export default async function ClientHomePage() {
+  const data = await getPublicMenu();
+  return (
+    <RestaurantBrandProvider nome={data.settings.nome} logoUrl={data.settings.logo_url}>
+      <div className="min-h-screen"><MenuBrowser {...data}/></div>
+    </RestaurantBrandProvider>
+  );
+}
