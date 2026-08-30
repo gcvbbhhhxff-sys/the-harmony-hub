559a5eb0b2510ee3b3b2ff74da8a90ac651c0698
gcvbbhhhxff-sys <gcvbbhhhxff@gmail.com>
2026-08-30 17:05:15 -0300

    fix: finish cart and checkout address flow

--- src/app/(cliente)/carrinho/page.tsx
+++ src/app/(cliente)/carrinho/page.tsx
@@ -1,4 +1,1 @@
-import { getPublicMenu } from "@/server/menu";
-import CartPageClient from "@/components/cliente/cart-page-client";
-export const dynamic="force-dynamic";
-export default async function CartPage(){const {optionGroups,options,addons,productAddons,settings}=await getPublicMenu();return <CartPageClient optionGroups={optionGroups} options={options} addons={addons} productAddons={productAddons} minimumOrder={Number((await import("@/server/menu")).getPublicMenu?0:0)} settings={settings}/>}
+import { getPublicMenu } from "@/server/menu";import CartPageClient from "@/components/cliente/cart-page-client";export const dynamic="force-dynamic";export default async function CartPage(){const {optionGroups,options,addons,productAddons,settings}=await getPublicMenu();return <CartPageClient optionGroups={optionGroups} options={options} addons={addons} productAddons={productAddons} minimumOrder={settings.valor_minimo_pedido}/>}
